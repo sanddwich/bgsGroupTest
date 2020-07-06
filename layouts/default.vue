d771d74ddca31fc36916837cf231d2cd5c28cdab
sanddwich <64834314+sanddwich@users.noreply.github.com>
2020-07-06 18:06:03 +0400

    06-07-2020_1

---
+++ layouts/default.vue
@@ -0,0 +1,15 @@
+<template>
+  <v-app>
+    <EnterForm />
+    <nuxt />
+  </v-app>
+</template>
+
+<script>
+import EnterForm from '@/components/enterForm';
+export default {
+  components: {
+    EnterForm
+  }
+}
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <h1>Hello</h1>
+  </div>
+</template>
+
+<script>
+// import Logo from '~/components/Logo.vue'
+// import VuetifyLogo from '~/components/VuetifyLogo.vue'
+
+export default {
+  components: {
+
+  }
+}
+</script>
